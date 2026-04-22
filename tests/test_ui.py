ec6b1c56fa93ed81d6ad9892404193813444c32d
Tanuja Biradar <tanujabiradar0010@gmail.com>
2026-04-22 08:42:11 +0530

    Skip Selenium for CI

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+pytest.skip("Skipping Selenium in CI", allow_module_level=True)
 
 def test_homepage():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
